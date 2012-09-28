4316af28f984d9dc140f89e25c95d8c7d3d23b27
Don Coleman <dcoleman@chariotsolutions.com>
2012-09-28 17:24:20 -0400

    resize webview to remove black bar when keyboard is removed. fix based on code from  @relikd

--- src/ios/KeyboardToolbarRemover.m
+++ src/ios/KeyboardToolbarRemover.m
@@ -3,25 +3,25 @@
 
 @implementation KeyboardToolbarRemover
 
-- (void) hide:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options 
+- (void) hide:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options
 {
     NSString* callbackId = [arguments objectAtIndex:0];
-            
+
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
-    
+
     CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    
+
     [super success:result callbackId:callbackId];
 }
 
 - (void) show:(NSMutableArray*)arguments withDict:(NSMutableDictionary*)options;
 {
     NSString* callbackId = [arguments objectAtIndex:0];
-    
+
     [[NSNotificationCenter defaultCenter] removeObserver:self];
-    
+
     CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-    
+
     [super success:result callbackId:callbackId];
 
 }
@@ -40,18 +40,34 @@
             break;
         }
     }
-    
+
     // Locate UIWebFormView.
-    for (UIView *possibleFormView in [keyboardWindow subviews]) {       
+    for (UIView *possibleFormView in [keyboardWindow subviews]) {
         // iOS 5 sticks the UIWebFormView inside a UIPeripheralHostView.
         if ([[possibleFormView description] rangeOfString:@"UIPeripheralHostView"].location != NSNotFound) {
-            
+
             // remove the border above the toolbar in iOS 6
             [[possibleFormView layer] setMasksToBounds:YES];
-            
+
             for (UIView *subviewWhichIsPossibleFormView in [possibleFormView subviews]) {
                 if ([[subviewWhichIsPossibleFormView description] rangeOfString:@"UIWebFormAccessory"].location != NSNotFound) {
                     [subviewWhichIsPossibleFormView removeFromSuperview];
+
+                    // http://stackoverflow.com/questions/10746998/phonegap-completely-removing-the-black-bar-from-the-iphone-keyboard/10796550#10796550
+                    UIScrollView *webScroll;
+                    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 5.0) {
+                        webScroll = [[self webView] scrollView];
+                    } else {
+                        webScroll = [[[self webView] subviews] lastObject];
+                    }
+
+                    CGRect newFrame = [webScroll frame];
+
+                    float accessoryHeight = [subviewWhichIsPossibleFormView frame].size.height;
+                    newFrame.size.height += accessoryHeight;
+
+                    [subviewWhichIsPossibleFormView removeFromSuperview];
+                    [webScroll setFrame:newFrame];
                 }
             }
         }
