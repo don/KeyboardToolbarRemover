db0282ae84b8eadd3fe4bb8d11285a53ff411906
Don Coleman <dcoleman@chariotsolutions.com>
2013-10-09 16:06:47 -0400

    indentation

--- www/keyboard_toolbar_remover.js
+++ www/keyboard_toolbar_remover.js
@@ -1,31 +1,31 @@
 /*global cordova*/
 'use strict';
 
-    var exec = cordova.require('cordova/exec'),
-        win = function (success) {
-            // do nothing, succeed quietly
-        };
-
-    function hide() {
-
-        var fail = function (error) {
-            console.log("Error hiding the web form accessory toolbar" + error);
-        };
-
-        exec(win, fail, "KeyboardToolbarRemover", "hide", []);
-    }
-
-
-    function show() {
-
-        var fail = function (error) {
-            console.log("Error enabling web form accessory toolbar" + error);
-        };
-
-        exec(win, fail, "KeyboardToolbarRemover", "show", []);
-    }
-
-    module.exports = {
-        hide: hide,
-        show: show
+var exec = cordova.require('cordova/exec'),
+    win = function (success) {
+        // do nothing, succeed quietly
     };
+
+function hide() {
+
+    var fail = function (error) {
+        console.log("Error hiding the web form accessory toolbar" + error);
+    };
+
+    exec(win, fail, "KeyboardToolbarRemover", "hide", []);
+}
+
+
+function show() {
+
+    var fail = function (error) {
+        console.log("Error enabling web form accessory toolbar" + error);
+    };
+
+    exec(win, fail, "KeyboardToolbarRemover", "show", []);
+}
+
+module.exports = {
+    hide: hide,
+    show: show
+};
